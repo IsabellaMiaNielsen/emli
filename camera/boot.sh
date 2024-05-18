08197f9ad8410e3da1a9229df18b5dc145db0b02
Isabella Mia Nielsen <isabellamianielsen@gmail.com>
2024-05-18 12:30:55 +0200

    boot added

---
+++ camera/boot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+sudo /etc/init.d/dnsmasq restart
+sudo systemctl stop bluetooth
+sudo systemctl disable bluetooth
+ 
